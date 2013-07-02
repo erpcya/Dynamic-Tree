e2945a77c6539a2ae9fedc84715e10bf14fb8c11
Yamel Senih <Yamel Senih@localhost>
2013-07-01 18:32:01 -0530

    Add Migrations Script

--- migration/mysql/003_Order_Field_Tree_mysql.sql
+++
@@ -1,5 +0,0 @@
--- May 20, 2013 12:15:11 PM VET
--- Dynamic Tree
-UPDATE AD_Field SET IsSameLine='N',Updated=TO_DATE('2013-05-20 12:15:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=8371
-;
-
---
+++ migration/oracle/900_Add_Table_To_Tree_oracle.sql
@@ -0,0 +1,115 @@
+-- Jul 1, 2013 7:19:54 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=855, IsDefault='N',Updated=TO_DATE('2013-07-01 19:19:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=112
+;
+
+-- Jul 1, 2013 7:20:59 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=866, IsDefault='N',Updated=TO_DATE('2013-07-01 19:20:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=111
+;
+
+-- Jul 1, 2013 7:21:14 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=857, IsDefault='N',Updated=TO_DATE('2013-07-01 19:21:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=113
+;
+
+-- Jul 1, 2013 7:21:28 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=854, IsDefault='N',Updated=TO_DATE('2013-07-01 19:21:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=114
+;
+
+-- Jul 1, 2013 7:21:34 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=116,Updated=TO_DATE('2013-07-01 19:21:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=10
+;
+
+-- Jul 1, 2013 7:21:46 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=188,Updated=TO_DATE('2013-07-01 19:21:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=20
+;
+
+-- Jul 1, 2013 7:22:05 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=291,Updated=TO_DATE('2013-07-01 19:22:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=40
+;
+
+-- Jul 1, 2013 7:22:15 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=155,Updated=TO_DATE('2013-07-01 19:22:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=50
+;
+
+-- Jul 1, 2013 7:22:31 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=208,Updated=TO_DATE('2013-07-01 19:22:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=30
+;
+
+-- Jul 1, 2013 7:22:44 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=203,Updated=TO_DATE('2013-07-01 19:22:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=60
+;
+
+-- Jul 1, 2013 7:23:00 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=230,Updated=TO_DATE('2013-07-01 19:23:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=100
+;
+
+-- Jul 1, 2013 7:24:00 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=316,Updated=TO_DATE('2013-07-01 19:24:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=110
+;
+
+-- Jul 1, 2013 7:24:39 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=291,Updated=TO_DATE('2013-07-01 19:24:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=103
+;
+
+-- Jul 1, 2013 7:24:48 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=274,Updated=TO_DATE('2013-07-01 19:24:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=109
+;
+
+-- Jul 1, 2013 7:24:59 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=188,Updated=TO_DATE('2013-07-01 19:24:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=101
+;
+
+-- Jul 1, 2013 7:25:08 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=155,Updated=TO_DATE('2013-07-01 19:25:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=104
+;
+
+-- Jul 1, 2013 7:25:45 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=208,Updated=TO_DATE('2013-07-01 19:25:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=102
+;
+
+-- Jul 1, 2013 7:25:55 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=203,Updated=TO_DATE('2013-07-01 19:25:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=105
+;
+
+-- Jul 1, 2013 7:26:02 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=230,Updated=TO_DATE('2013-07-01 19:26:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=108
+;
+
+-- Jul 1, 2013 7:26:48 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=855,Updated=TO_DATE('2013-07-01 19:26:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=115
+;
+
+-- Jul 1, 2013 7:27:05 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=857,Updated=TO_DATE('2013-07-01 19:27:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=118
+;
+
+-- Jul 1, 2013 7:27:32 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=866,Updated=TO_DATE('2013-07-01 19:27:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=116
+;
+
+-- Jul 1, 2013 7:27:49 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tree SET AD_Table_ID=854,Updated=TO_DATE('2013-07-01 19:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tree_ID=117
+;
+
